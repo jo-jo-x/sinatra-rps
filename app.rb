68fbba191acbbf1e2f46fef7c1e5eba1c0d9606f
Maurice Ford <mauriceford@gmail.com>
2025-03-11 20:05:57 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -2,8 +2,65 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+  rps = ["rock", "paper", "scissors"]
+  @hands = rps.sample
+
+  @winner = { 
+  "rock" => "scissors", 
+  "paper" => "rock", 
+  "scissors" => "paper" 
+  }
+  if @winner["rock"] == @hands
+    @result = "We win!"
+  elsif @hands == "rock"
+    @result = "We tied!"
+  else
+    @result = "We lost!"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  rps = ["rock", "paper", "scissors"]
+  @hands = rps.sample
+
+  @winner = { 
+  "rock" => "scissors", 
+  "paper" => "rock", 
+  "scissors" => "paper" 
+  }
+  if @winner["paper"] == @hands
+    @result = "We win!"
+  elsif @hands == "paper"
+    @result = "We tied!"
+  else
+    @result = "We lost!"
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  rps = ["rock", "paper", "scissors"]
+  @hands = rps.sample
+
+  @winner = { 
+  "rock" => "scissors", 
+  "paper" => "rock", 
+  "scissors" => "paper" 
+  }
+  if @winner["scissors"] == @hands
+    @result = "We win!"
+  elsif @hands == "scissors"
+    @result = "We tied!"
+  else
+    @result = "We lost!"
+  end
+
+  erb(:scissors)
 end
